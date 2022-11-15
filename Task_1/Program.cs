54f6319b7ec3c71425113800e9e8351d03f7cd8e
Funt <funt_88@mail.ru>
2022-11-16 02:03:34 +0300

    дополнительные исправления к задаче 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -3,10 +3,18 @@
 Console.WriteLine("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 
-if (number > 99 && number < 1000)
+if (number > 99 && number < 1000 || number < -99 && number > -1000)
 {
-    int number2 = number / 10 % 10;
-    Console.WriteLine($"Вторая цифра: {number2}");
+    if (number > 0)
+    {
+        int number2 = number / 10 % 10;
+        Console.WriteLine($"Вторая цифра: {number2}");
+    }
+    if (number < 0)
+    {
+        int number2 = -number / 10 % 10;
+        Console.WriteLine($"Вторая цифра: {number2}");
+    }
 }
 else
 {
