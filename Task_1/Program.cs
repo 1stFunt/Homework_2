aa7f547b71405115867956d197f10142acc034e8
Funt <funt_88@mail.ru>
2022-11-16 18:57:13 +0300

    оптимизация задачи 1

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -7,13 +7,13 @@ if (number > 99 && number < 1000 || number < -99 && number > -1000)
 {
     if (number > 0)
     {
-        int number2 = number / 10 % 10;
-        Console.WriteLine($"Вторая цифра: {number2}");
+        number = number / 10 % 10;
+        Console.WriteLine($"Вторая цифра: {number}");
     }
-    else if (number < 0)
+    if (number < 0)
     {
-        int number2 = -number / 10 % 10;
-        Console.WriteLine($"Вторая цифра: {number2}");
+        number = -number / 10 % 10;
+        Console.WriteLine($"Вторая цифра: {number}");
     }
 }
 else
