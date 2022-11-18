d68e0496073c1a2a6f75fdcd43390e2d6df45752
Funt <116380445+1stFunt@users.noreply.github.com>
2022-11-19 02:19:37 +0300

    Дополнительные варианты решения 2ой задачи

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -33,3 +33,53 @@ while (number > 999 || number < -999)
         Console.WriteLine($"Третья цифра: {number}");
     }
 }
+
+﻿/* Второй вариант:
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int searchNumber = 3;
+int temp = number;
+int count = 0;
+if (number < 0)
+{
+    number *= -1;
+    temp = number;
+}
+while (temp > 0)
+{
+    {
+        temp /= 10;
+        count++;
+    }
+
+}
+while (count > searchNumber)
+{
+    {
+        number /= 10;
+        searchNumber++;
+    }
+}
+if (count < 3)
+{
+    Console.WriteLine("третьего числа нет");
+}
+else
+{
+    Console.WriteLine(number % 10);
+} */
+
+﻿/* Третий вариант:
+
+Console.Write("Введи число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberText = Convert.ToString(number);
+if (numberText.Length > 2)
+{
+    Console.WriteLine("Третья цифра: " + numberText[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}*/
